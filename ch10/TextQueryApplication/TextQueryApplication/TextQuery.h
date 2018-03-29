ade337707ba99ea0ac55108a2a1a7c382ce71894
EDG-Zola <wellzengwei@163.com>
2018-03-30 01:54:54 +0800

    添加了成员函数，但仍然有bug

--- ch10/TextQueryApplication/TextQueryApplication/TextQuery.h
+++ ch10/TextQueryApplication/TextQueryApplication/TextQuery.h
@@ -15,11 +15,32 @@ public:
 		store_file(is);
 		build_map();
 	}
-	set<line_no> run_query(const string&) const;
-	string text_line(line_no) const;
+	set<line_no> run_query(const string&) const{
+		map<string, set<line_no> >::const_iterator
+			loc = word_map.find(query_word);
+		if (loc == word_map.end())
+			return sec<line_no>();
+		else
+			return loc->second;
+	};
+	string text_line(line_no) const{
+		if (line < lines_of_text.size())
+			return lines_of_text[line];
+		throw out_of_range("line number out of range");
+	};
 private:
-	void store_file(ifstream& is);
-	void build_map();
+	void store_file(ifstream& is){
+		string textline;
+		while(GetInputState(is, textline))
+			lines_of_text.push_back(textline);
+	};
+	void build_map(){
+		for (line_no line_num = 0; line_num  != lines_of_text.size(); ++line_num){
+			istringstream line(lines_of_text[line_num]);
+			string word;
+			while(line << word)
+				word_map[word].insert(line_num);
+	};
 	vector<string> lines_of_text;
 	map<string, set<line_no> > word_map;
 };
