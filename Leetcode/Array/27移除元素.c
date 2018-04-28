9cdbb236b4b96d259528911cae0506d5eeac8bbd
EDG-Zola <wellzengwei@163.com>
2018-04-28 19:06:31 +0800

    remove

--- Leetcode/Array/27移除元素.c
+++ Leetcode/Array/27移除元素.c
@@ -2,8 +2,6 @@ class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
         int len = nums.size();
-        if(len == 0)
-            return nums.size();
         for(int i=0; i != len; ){
             if(nums[i] == val){
                 nums.erase(nums.begin()+i);
